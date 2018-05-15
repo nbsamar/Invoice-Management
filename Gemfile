08b0e6dd273f840778f48d71684cd5ecbc0d8d38
nbsamar <nayab.samar@in.reportbee.com>
2018-05-15 13:52:06 +0000

    Routes, Bootstrap 4 gem, invoices view and controller methods

--- Gemfile
+++ Gemfile
@@ -33,6 +33,9 @@ gem 'jbuilder', '~> 2.5'
 # Use Capistrano for deployment
 # gem 'capistrano-rails', group: :development
 
+# Bootstrap 4
+gem 'bootstrap', '~> 4.1', '>= 4.1.1'
+
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
--- app/controllers/invoices_controller.rb
+++ app/controllers/invoices_controller.rb
@@ -1,2 +1,21 @@
 class InvoicesController < ApplicationController
+    def new
+        @invoice = Invoice.new
+    end
+
+    def create
+        @invoice = Invoice.new(invoice_params)
+        if @invoice.save
+            flash[:notice] = "Invoice Created"
+            redirect_to invoices_path
+        else
+            flash[:error] = @invoice.errors.full_messages.join(', ').html_safe
+            render :new
+        end
+    end
+
+    def invoice_params
+        params.require(:invoice).permit(:brand_manager, :narration, :amount, :customer_name, :reference, :invoice_date)
+    end
+
 end
