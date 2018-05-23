d99eca13534cb850e5ee4ae7db6b2f0058f0cd3a
nbsamar <nayab.samar@in.reportbee.com>
2018-05-23 13:48:39 +0000

    collection create method bug fix

--- app/controllers/collections_controller.rb
+++ app/controllers/collections_controller.rb
@@ -1,10 +1,24 @@
 class CollectionsController < ApplicationController
-    def new
-        @invoice = Invoice.find(params[:id])
-        @collections = @invoice.collections
-        @amounts = []
-        @collections.each do |collection|
-            @amounts << collection.amount
+    # def new
+    #     @invoice = Invoice.find(params[:id])
+    #     @collections = @invoice.collections
+    #     @amounts = []
+    #     @collections.each do |collection|
+    #         @amounts << collection.amount
+    #     end
+    # end
+    def create
+        @collection = Collection.new(collection_params)
+        if @collection.save
+            flash[:notice] = "Collection Created"
+            redirect_to invoices_path
+        else
+            flash[:error] = @collection.errors.full_messages.join(', ').html_safe
+            render :new
         end
     end
+    def collection_params
+        params.require(:collection).permit(:amount, :reference, :collection_date)
+    end
+    
 end
